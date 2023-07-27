9c1ee37d834ec3e0a81d00d4b151e68d023e58ef
Olivia Qin <oliveqy@berkeley.edu>
2023-07-26 21:19:48 -0400

    fixed _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Nav from '../components/Nav/Nav'
-import Footer from '../components/Footer/Footer'
+import Nav from '../components/Nav/nav'
+import Footer from '../components/Footer/footer'
 
 export default function App({ Component, pageProps }) {
   return (
