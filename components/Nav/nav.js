e004e3e4e4448caa0371f7adf8819736edf2280d
Olivia Qin <oliveqy@berkeley.edu>
2023-07-26 21:55:06 -0400

    removed button

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -18,19 +18,6 @@ export default function Nav() {
       <nav aria-label="Global" class="hidden md:block">
       </nav>
       <div class="flex items-center gap-4">
-        <button id='theme-toggle' type='button'>
-        <label for="AcceptConditions" class="relative h-8 w-14 cursor-pointer">
-            <input type="checkbox" id="AcceptConditions" class="peer sr-only" />
-
-            <span
-              class="absolute inset-0 rounded-full bg-gray-300 transition peer-checked:bg-green-500"
-            ></span>
-
-            <span
-              class="absolute inset-y-0 start-0 m-1 h-6 w-6 rounded-full bg-white transition-all peer-checked:start-6"
-            ></span>
-          </label>
-        </button>
         <ul class="pt-8 space-y-1">
         <li>
         </li>
