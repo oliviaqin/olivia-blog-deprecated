ba4c6abb45785e7437b0846edf26e65de87c73d4
Olivia Qin <oliveqy@berkeley.edu>
2023-09-29 16:38:06 -0400

    more pics

--- pages/random.js
+++ pages/random.js
@@ -23,15 +23,6 @@ export default function Random() {
       <div class="relative justify-center group">
         <img
           class="w-full p-3.5 group-hover:opacity-50 duration-300"
-          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1695758295/IMG_7765_dpgu0q.jpg"
-        />
-        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
-          egg, cheese, mushrooms, carrots, cherry tomatoes, broccoli, avocado
-        </div>
-      </div>
-      <div class="relative justify-center group">
-        <img
-          class="w-full p-3.5 group-hover:opacity-50 duration-300"
           src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1695758295/IMG_7627_fm61ft.jpg"
         />
         <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
@@ -45,6 +36,78 @@ export default function Random() {
         />
         <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
           omelette, avocado, salmon, broccoli, sesame seeds
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696015639/IMG_8247_vdmtnn.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          Yum pastries
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1695758295/IMG_7765_dpgu0q.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          egg, cheese, mushrooms, carrots, cherry tomatoes, broccoli, avocado
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696015640/IMG_8053_ntn5sx.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          Versailles
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696016263/IMG_8523_uopylp.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          Friends
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696016265/IMG_4836_yoddwh.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          california
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696019549/Birds_kgjyc5.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          clouds
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696019550/Studies_yo2nur.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          study #1
+        </div>
+      </div>
+      <div class="relative justify-center group">
+        <img
+          class="w-full p-3.5 group-hover:opacity-50 duration-300"
+          src="https://res.cloudinary.com/ddfmg0d4v/image/upload/v1696019552/Studies_2_zza12z.jpg"
+        />
+        <div class="absolute inset-1/3 z-10 opacity-0 group-hover:opacity-100 text-black duration-300">
+          study #2
         </div>
       </div>
     </div>
