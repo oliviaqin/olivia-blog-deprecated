adf01a8603c9a91db7cc22c5730f52428cd41e62
Olivia Qin <oliveqy@berkeley.edu>
2024-02-22 16:04:37 -0500

    reset to projects for now

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -27,23 +27,23 @@ export default function Nav() {
                   about
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <a
                   href="https://drive.google.com/file/d/1TsKMOBkj0pSWjrY5InJspngUAOOTg0Ww/view?usp=sharing"
                   class="block text-sm rounded-lg px-4 py-2 text-gray-500 hover:text-gray-700 hover:underline lg:text-base"
                 >
                   cv
                 </a>
-              </li>
+              </li> */}
 
-              {/* <li>
+              <li>
                 <Link
                   href="/projects"
                   class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:underline lg:text-base"
                 >
                   projects
                 </Link>
-              </li> */}
+              </li>
 
               <li>
                 <a
