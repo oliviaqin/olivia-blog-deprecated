c2de61df765544ece6d0434ecdf6fba69e47037c
Olivia Qin <oliveqy@berkeley.edu>
2024-03-05 14:11:31 -0500

    updating nav

--- components/Nav/nav.js
+++ components/Nav/nav.js
@@ -27,19 +27,11 @@ export default function Nav() {
                   about
                 </Link>
               </li>
-              {/* <li>
-                <a
-                  href="https://drive.google.com/file/d/1TsKMOBkj0pSWjrY5InJspngUAOOTg0Ww/view?usp=sharing"
-                  class="block text-sm rounded-lg px-4 py-2 text-gray-500 hover:text-gray-700 hover:underline lg:text-base"
-                >
-                  cv
-                </a>
-              </li> */}
 
               <li>
                 <Link
                   href="/projects"
-                  class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:underline lg:text-base"
+                  class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:text-gray-700 hover:underline lg:text-base"
                 >
                   projects
                 </Link>
@@ -54,14 +46,23 @@ export default function Nav() {
                 </a>
               </li>
 
-              {/* <li>
+              <li>
+                <a
+                  href="https://drive.google.com/file/d/1TsKMOBkj0pSWjrY5InJspngUAOOTg0Ww/view?usp=sharing"
+                  class="block text-sm rounded-lg px-4 py-2 text-gray-500 hover:text-gray-700 hover:underline lg:text-base"
+                >
+                  cv
+                </a>
+              </li>
+
+              <li>
                 <a
                   href="https://extraolivespls.substack.com/"
                   class="block text-sm rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:underline lg:text-base"
                 >
                   blog
                 </a>
-              </li> */}
+              </li>
             </ul>
           </div>
         </div>
