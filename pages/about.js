9a631cd60de55034ddfa6c8f281a6ae959669a85
Olivia Qin <olivia.qin@datadoghq.com>
2024-05-09 20:29:18 -0400

    move book section

--- pages/about.js
+++ pages/about.js
@@ -53,44 +53,6 @@ export default function About() {
               Outside of work, I enjoy traveling, reading, running,
               snowboarding, and aesthetic things 🌱
             </p>
-            <p class="mt-4 text-gray-700">
-              📚 Some book reccommendations recently:
-              <br />
-              <a
-                class="underline"
-                href="https://www.goodreads.com/book/show/31138556-homo-deus"
-              >
-                Homo Deus - Yuval Noah Harari
-              </a>
-              <br />{" "}
-              <a
-                class="underline"
-                href="https://www.goodreads.com/book/show/6149.Beloved?from_search=true&from_srp=true&qid=ixzRax9Vfk&rank=1"
-              >
-                Beloved - Toni Morrison
-              </a>{" "}
-              <br />{" "}
-              <a
-                class="underline"
-                href="https://www.goodreads.com/book/show/20518872-the-three-body-problem"
-              >
-                Three Body Problem - Cixin Liu
-              </a>{" "}
-              <br />
-              <a
-                class="underline"
-                href="https://www.goodreads.com/author/show/130698.Ted_Chiang"
-              >
-                Ted Chiang
-              </a>
-              <br />{" "}
-              <a
-                class="underline"
-                href="https://www.goodreads.com/book/show/60321447-chip-war"
-              >
-                The Chip War - Chris Miller
-              </a>
-            </p>
           </div>
         </div>
       </div>
---
+++ pages/shelf.js
@@ -0,0 +1,44 @@
+
+
+export default function Shelf() {
+
+    return (<p class="ml-8 mt-4 text-gray-700">
+        Books: 
+        <br />
+            <a
+            class="underline"
+            href="https://www.goodreads.com/book/show/31138556-homo-deus"
+            >
+            Homo Deus - Yuval Noah Harari
+            </a>
+        <br />{" "}
+            <a
+            class="underline"
+            href="https://www.goodreads.com/book/show/6149.Beloved?from_search=true&from_srp=true&qid=ixzRax9Vfk&rank=1"
+            >
+            Beloved - Toni Morrison
+            </a>{" "}
+        <br />{" "}
+            <a
+            class="underline"
+            href="https://www.goodreads.com/book/show/20518872-the-three-body-problem"
+            >
+            Three Body Problem - Cixin Liu
+            </a>{" "}
+        <br />
+            <a
+            class="underline"
+            href="https://www.goodreads.com/author/show/130698.Ted_Chiang"
+            >
+            Ted Chiang
+            </a>
+        <br />{" "}
+            <a
+            class="underline"
+            href="https://www.goodreads.com/book/show/60321447-chip-war"
+            >
+            The Chip War - Chris Miller
+            </a>
+    </p> )
+
+}
