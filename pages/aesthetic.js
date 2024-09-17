90da54bf15aeba7b79e41a40a6cacacfb449d444
Olivia Qin <olivia.qin@datadoghq.com>
2024-09-17 20:36:43 +0200

    update const imports

--- pages/aesthetic.js
+++ pages/aesthetic.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import imageList from './data/_image_list';
+import { imageList } from './data/_image_list';
 
 export default function Aesthetic() {
   const [loadedStates, setLoadedStates] = useState(
