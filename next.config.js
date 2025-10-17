68eb3f928fa41a66f68837284b6b1b0f791b022f
Olivia Qin <olivia.qin@datadoghq.com>
2025-10-17 07:29:14 -0400

    upgrade workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enable static HTML export
   images: {
     domains: ['i.imgur.com', 'res.cloudinary.com'],
+    unoptimized: true, // Required for static export
   },
 };
 
